35d612c1672c01716e6cdcdef5f7c2610430a778
Alastair Coote <alastair.coote@gmail.com>
2017-01-11 10:29:56 -0500

    in-progress importscripts work

--- 2017011100000000.sql
+++ 2017011100000000.sql
@@ -1,11 +1,10 @@
-CREATE TABLE "cache" (
-    "service_worker_url" TEXT NOT NULL,
-    "cache_id" TEXT NOT NULL,
+CREATE TABLE "import_scripts" (
+    "service_worker_id" integer NOT NULL,
     "resource_url" TEXT NOT NULL,
     "last_modified" integer,
     "etag" text,
     "contents" blob NOT NULL,
     "headers" text NOT NULL,
     "status" integer NOT NULL,
-    PRIMARY KEY("service_worker_url", "cache_id","resource_url")
+    PRIMARY KEY("service_worker_id", "resource_url")
 );
---
+++ hybrid/ImportScriptsHandler.swift
@@ -0,0 +1,19 @@
+//
+//  ImportScriptsHandler.swift
+//  hybrid
+//
+//  Created by alastair.coote on 11/01/2017.
+//  Copyright © 2017 Alastair Coote. All rights reserved.
+//
+
+import Foundation
+
+class ImportScriptsHandler {
+    
+    let serviceWorker:ServiceWorkerInstance
+    
+    
+    init(serviceWorker:ServiceWorkerInstance) {
+        self.serviceWorker = serviceWorker
+    }
+}
